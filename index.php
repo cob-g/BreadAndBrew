2423bb1dd7cb0467ca989a41cf7b5311f289af92
Glenn Mark Jacob <136462146+cob-g@users.noreply.github.com>
2023-11-08 18:33:45 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bread And Brew</title>
+
+     <link href="styles.css"rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css?family=Oleo+Script+Swash+Caps" rel="stylesheet">
+    <!-- font-family -->
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- bootstrap link -->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- font awesome -->
+  
+</head>
+<body>
+            <!-- navbar -->
+            <div class="container-fluid p-0">
+                <!-- first child -->
+            <nav class="navbar navbar-expand-lg navbar-light bg-body-transparent">
+                <div class="container-fluid">
+                <img src="./img/logo.png" alt="cafelogo" class="logo">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">                        
+                            <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#">Product</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#">Register</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"></i><sup>1</sup></a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#">Total Pricee:100/-</a>
+                            </li>    
+                        </ul>
+                        <form class="d-flex" role="search">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-light" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+
+            <!-- second child -->
+            <nav class="navbar navbar-expand-lg navbar-dark bg-body-transparent navbar-black-border">
+                <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Welcome Guest</a>
+                    </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <!-- third child -->
+            <!-- <div class="bg-transparent text-white"> 
+                <h3 class="text-center">Your Pastry and Brew</h3>
+                <p class="text-center">The destination for coffee and bread</p>
+            </div>-->
+            <div class="content"> 
+                <h1 class="title">Bread & Brew</h1>
+                <p class="par1">lorem ipsum Font Changer Copy Paste as the name suggests easily allows you to copy your converted fonts to clipboard. This can be used just by pressing "ctrl+V" on your keyboard. While on mobile these lorem ipsum fonts can be used by just pressing the screen long enough to display an option "paste" and then clicking it. lorem ipsum Font Changer Copy Paste as the name suggests easily allows you to copy your converted fonts to clipboard. This can be used just by pressing "ctrl+V" on your keyboard. While on mobile these lorem ipsum fonts can be used by just pressing the screen long enough to display an option "paste" and then clicking it.</p>
+                <a class="btn-explore btn-transparent" href="#Menu">Explore Menu</a>
+                <div class="bg-transparent text-center transparent-bg">
+
+                    <!-- last child -->
+             <p>All rights reserved © by JMC-2023</p>
+            </div>
+            </div>
+            </div>
+
+            <div id="Menu" class="menu-container">
+                <!-- first child -->
+                <div class="row">
+                    <div class="col-md-10">
+                <!-- products -->
+                    <div class="row">
+
+                        <div class="col-md-4 mb-3 mt-3">
+                            <div class="card text-center" style=" width: 250px;">
+                                <img src="./img/coffee1.png" class="card-img-top" alt="..."  >                    
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on.</p>
+                                    <a href="#" class="btn btn-dark">Add to cart</a>
+                                    <a href="#" class="btn btn-dark">View more</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4 mb-3 mt-3">
+                            <div class="card text-center" style=" width: 250px;">
+                                <img src="./img/coffee1.png" class="card-img-top" alt="..."  >                    
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on.</p>
+                                    <a href="#" class="btn btn-dark">Add to cart</a>
+                                    <a href="#" class="btn btn-dark">View more</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4 mb-3 mt-3">
+                            <div class="card text-center" style=" width: 250px;">
+                                <img src="./img/coffee1.png" class="card-img-top" alt="..."  >                    
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on.</p>
+                                    <a href="#" class="btn btn-dark">Add to cart</a>
+                                    <a href="#" class="btn btn-dark">View more</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4 mb-3">
+                            <div class="card text-center" style=" width: 250px;">
+                                <img src="./img/coffee1.png" class="card-img-top" alt="..."  >                    
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on.</p>
+                                    <a href="#" class="btn btn-dark">Add to cart</a>
+                                    <a href="#" class="btn btn-dark">View more</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        
+                        <div class="col-md-4 mb-3">
+                            <div class="card text-center" style=" width: 250px;">
+                                <img src="./img/coffee1.png" class="card-img-top" alt="..."  >                    
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on.</p>
+                                    <a href="#" class="btn btn-dark">Add to cart</a>
+                                    <a href="#" class="btn btn-dark">View more</a>
+                                </div>
+                            </div>
+                        </div>
+                         
+                        <div class="col-md-4 mb-3">
+                            <div class="card text-center" style=" width: 250px;">
+                                <img src="./img/coffee1.png" class="card-img-top" alt="..."  >                    
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on.</p>
+                                    <a href="#" class="btn btn-dark">Add to cart</a>
+                                    <a href="#" class="btn btn-dark">View more</a>
+                                </div>
+                            </div>
+                        </div>
+                      
+                <div class="col-md-2">
+                <!-- sidenav -->
+                <h1>hey</h1>
+                    </div>
+                </div>
+
+                </div>
+            </div>
+        
+            
+
+
+    <!-- bootstrap js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
